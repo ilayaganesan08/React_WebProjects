b41f537f179d1ce0428057d582fcbfdf4036fc89
K.Ilayaganesan <139547525+ilayaganesan08@users.noreply.github.com>
2024-06-29 21:33:40 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import Form from './Form'
+// import './Form.css'
+// import App from './App.jsx'
+// import QrCode from './QrCode'
+// import './QrCode.css'
+// import './index.css'
+import UserCards from './UserCards'
+import './Cards.css'
+// import AdviceApp from './AdviceApp'
+// import './Advice.css'
+// import Weather from './Weather'
+// import './weather.css'
+// import Bmi from './Bmi.jsx';
+// import './bmi.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <App /> */}
+    {/* <QrCode/> */}
+    <UserCards/>
+    {/* <Form/> */}
+    {/* <AdviceApp/> */}
+    {/* <Weather/> */}
+    {/* <Bmi/> */}
+  </React.StrictMode>,
+)
